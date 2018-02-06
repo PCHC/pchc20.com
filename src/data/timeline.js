b964c96b990778a8f12a5285d390adc2ef6cfb65
Chris Violette <cvioletterun@gmail.com>
2018-02-06 14:00:24 -0500

    Updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,18 +2,18 @@ export default function() {
   return [
     {
       year: 1998,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.',
-      image: '400x400.png',
+      description: 'Opened at 992 Union Street in Bangor with 6 exam rooms.',
+      image: '1998.jpg',
     },
     {
       year: 1999,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.',
-      image: '400x400.png',
+      description: 'FQHC Look-a-Like status awarded by the Health Resources and Services Administration.',
+      image: '1999.jpg',
     },
     {
-      year: 2000,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.',
-      image: '400x400.png',
+      year: 2001,
+      description: 'Moved to 1012 Union Street in Bangor with 20 exam rooms in 7,500 square feet.',
+      image: '2001.jpg',
     }
   ]
 }
