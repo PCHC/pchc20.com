a81362a68931c5bb79360727ccda85ccb63d30ed
Chris Violette <cvioletterun@gmail.com>
2018-02-21 12:21:22 -0500

    Updating timeline content & styling

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,19 +1,104 @@
 export default function() {
   return [
     {
+      year: 1997,
+      description: 'PCHC incorporated and Board of Directors formed',
+      image: '1998.jpg',
+    },
+    {
       year: 1998,
-      description: 'Opened at 992 Union Street in Bangor with 6 exam rooms.',
+      description: 'Opened at 992 Union Street in Bangor with 6 exam rooms',
       image: '1998.jpg',
     },
     {
       year: 1999,
-      description: 'FQHC Look-a-Like status awarded by the Health Resources and Services Administration.',
+      description: 'FQHC Look-a-Like status awarded by the Health Resources and Services Administration',
       image: '1999.jpg',
     },
     {
       year: 2001,
-      description: 'Moved to 1012 Union Street in Bangor with 20 exam rooms in 7,500 square feet.',
+      description: 'Moved to 1012 Union Street in Bangor with 20 exam rooms in 7,500 square feet',
       image: '2001.jpg',
-    }
+    },
+    {
+      year: 2002,
+      description: 'Board of Directors hires CEO; started with 20 staff and 4 primary care providers',
+      image: '2001.jpg',
+    },
+    {
+      year: 2003,
+      description: 'Clinic is named Penobscot Community Health Center and hires the first pharmacist',
+      image: '2001.jpg',
+    },
+    {
+      year: 2004,
+      description: '100 employees milestone.',
+      image: '2001.jpg',
+    },
+    {
+      year: 2005,
+      description: 'Old Town Family Practice and Penobscot Pediatrics join PCHC',
+      image: '2001.jpg',
+    },
+    {
+      year: 2006,
+      description: 'Capehart Community Health Center and Summer Street Health Center open',
+      image: '2001.jpg',
+    },
+    {
+      year: 2007,
+      description: 'Helen Hunt Health Center opens in Old Town',
+      image: '2001.jpg',
+    },
+    {
+      year: 2008,
+      description: 'Brewer Community Clinic opens, and PCHC earns Joint Commission accreditation',
+      image: '2001.jpg',
+    },
+    {
+      year: 2009,
+      description: 'General practice dental residency program begins',
+      image: '2001.jpg',
+    },
+    {
+      year: 2010,
+      description: 'Audiology and Speech Therapy at the Warren Center opens, Brewer Medical Center opens, and a shelter and clinic open at the Hope House',
+      image: '2001.jpg',
+    },
+    {
+      year: 2011,
+      description: 'PCHC receives a $3 million HRSA Teaching Health Center grant; Pediatric Dental, Community Pharmacy, and Nurse Practitioner residency programs start',
+      image: '2001.jpg',
+    },
+    {
+      year: 2012,
+      description: 'Unlimited Solutions Clubhouse opens on Summer Street in Bangor.',
+      image: '2001.jpg',
+    },
+    {
+      year: 2013,
+      description: 'Seaport Community Health Center joins PCHC; Orthodontics program begins at Dental Center; Moved billing, records, and referrals departments to Services Center on Odlin Road in Bangor',
+      image: '2001.jpg',
+    },
+    {
+      year: 2014,
+      description: 'Winterport Community Health Center and Jackman Community Health Center join PCHC',
+      image: '2001.jpg',
+    },
+    {
+      year: 2015,
+      description: 'Led initiation of state-wide MaineCare and Medicare Accountable Care Organization; a new clinic and 24 more Transitional Housing beds open at Hope House',
+      image: '2001.jpg',
+    },
+    {
+      year: 2016,
+      description: 'Downeast School Dental Clinic opens; Oral surgery starts at Dental Center; and Chiropractic Services begin in Bangor and Brewer',
+      image: '2001.jpg',
+    },
+    {
+      year: 2017,
+      description: 'Hope House gains Opioid Health Home designation; Open community rapid-access Suboxone treatment begins in Brewer',
+      image: '2001.jpg',
+    },
   ]
 }
