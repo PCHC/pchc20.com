2904c162800f366a258be5bd2fd860bd9bade8f9
Chris Violette <cvioletterun@gmail.com>
2018-02-21 12:21:34 -0500

    Updating services

--- src/data/services.js
+++ src/data/services.js
@@ -1,80 +1,96 @@
 export default function() {
   return [
     {
+      name: "After-Hours Care",
+      link: "https://pchc.com/services/after-hours-care/",
+    },
+    {
       name: "Audiology",
-      link: null,
+      link: "https://pchc.com/services/audiology/",
     },
     {
       name: "Care Management",
-      link: null,
+      link: "https://pchc.com/services/care-management/",
     },
     {
       name: "Chiropractic",
-      link: null,
+      link: "https://pchc.com/services/chiropractic/",
     },
     {
       name: "Community Care & Geriatrics",
-      link: null,
+      link: "https://pchc.com/services/community-care-geriatrics/",
+    },
+    {
+      name: "Community Care Team (CCT)",
+      link: "https://pchc.com/services/community-care-team/",
     },
     {
       name: "Dental Services",
-      link: null,
+      link: "https://pchc.com/services/dental-services/",
     },
     {
       name: "Family Medicine",
-      link: null,
+      link: "https://pchc.com/services/family-medicine/",
     },
     {
       name: "Health Care for the Homeless",
-      link: null,
+      link: "https://pchc.com/services/health-care-for-the-homeless/",
     },
     {
       name: "Laboratory Services",
-      link: null,
+      link: "https://pchc.com/services/laboratory-services/",
+    },
+    {
+      name: "Medical Specialists",
+      link: "https://pchc.com/services/medical-specialists/",
     },
     {
       name: "Mental Health",
-      link: null,
+      link: "https://pchc.com/services/mental-health/",
     },
     {
       name: "Nutrition",
-      link: null,
+      link: "https://pchc.com/services/nutrition/",
     },
     {
       name: "Osteopathic Manipulation Therapy",
-      link: null,
+      link: "https://pchc.com/services/osteopathic-manipulation-therapy/",
     },
     {
       name: "Pediatrics",
-      link: null,
+      link: "https://pchc.com/services/pediatrics/",
     },
     {
       name: "Pharmacy",
-      link: null,
+      link: "https://pchc.com/services/pharmacy/",
+    },
+    {
+      name: "Physical Therapy",
+      link: "https://pchc.com/services/physical-therapy/",
     },
     {
       name: "Podiatry",
-      link: null,
+      link: "https://pchc.com/services/podiatry/",
     },
     {
       name: "Recovery",
-      link: null,
+      link: "https://pchc.com/services/recovery/",
     },
     {
       name: "Speech Therapy",
-      link: null,
+      link: "https://pchc.com/services/speech-therapy/",
     },
     {
       name: "Walk-In Care",
-      link: null,
+      link: "https://pchc.com/services/walk-in-care/",
     },
     {
-      name: "Women's Health",
-      link: null,
+      name: "Women’s Health",
+      link: "https://pchc.com/services/womens-health/",
     },
     {
       name: "X-Ray",
-      link: null,
+      link: "https://pchc.com/services/x-ray/",
     },
   ]
 }
