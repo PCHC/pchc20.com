08738c123c8d73084401f476b089e6f5c0bf1323
Chris Violette <cvioletterun@gmail.com>
2018-02-22 12:58:10 -0500

    added rel="noopener noreferrer" to links opening in new windows

--- src/components/common/LinkIcons.js
+++ src/components/common/LinkIcons.js
@@ -4,32 +4,32 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 const LinkIcons = () => (
   <ul className="link-icons">
     <li>
-      <a target="_blank" href="http://www.facebook.com/PenobscotCommunityHealthCare" className="color__facebook" title="Facebook" aria-label="Facebook">
+      <a target="_blank" rel="noopener noreferrer" href="http://www.facebook.com/PenobscotCommunityHealthCare" className="color__facebook" title="Facebook" aria-label="Facebook">
         <FontAwesomeIcon icon={["fab", "facebook"]}/>
       </a>
     </li>
     <li>
-      <a target="_blank" href="https://www.twitter.com/PCHCare" className="color__twitter" title="Twitter" aria-label="Twitter">
+      <a target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/PCHCare" className="color__twitter" title="Twitter" aria-label="Twitter">
         <FontAwesomeIcon icon={["fab", "twitter"]}/>
       </a>
     </li>
     <li>
-      <a target="_blank" href="https://www.instagram.com/PCHCare" className="color__instagram" title="Instagram" aria-label="Instagram">
+      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/PCHCare" className="color__instagram" title="Instagram" aria-label="Instagram">
         <FontAwesomeIcon icon={["fab", "instagram"]}/>
       </a>
     </li>
     <li>
-      <a target="_blank" href="https://www.linkedin.com/company/5211558" className="color__linkedin" title="LinkedIn" aria-label="LinkedIn">
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/5211558" className="color__linkedin" title="LinkedIn" aria-label="LinkedIn">
         <FontAwesomeIcon icon={["fab", "linkedin"]}/>
       </a>
     </li>
     <li>
-      <a target="_blank" href="http://www.youtube.com/pchcvideo" className="color__youtube" title="YouTube" aria-label="YouTube">
+      <a target="_blank" rel="noopener noreferrer" href="http://www.youtube.com/pchcvideo" className="color__youtube" title="YouTube" aria-label="YouTube">
         <FontAwesomeIcon icon={["fab", "youtube"]}/>
       </a>
     </li>
     <li>
-      <a target="_blank" href="https://pchc.com/" className="color__pchc" title="PCHC.com" aria-label="PCHC.com">
+      <a target="_blank" rel="noopener noreferrer" href="https://pchc.com/" className="color__pchc" title="PCHC.com" aria-label="PCHC.com">
         <FontAwesomeIcon icon="globe"/>
       </a>
     </li>
